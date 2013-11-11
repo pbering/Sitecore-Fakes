6d77d21520d4fd05097e86449fd77565000a9a6d
Per Manniche Bering <pmbering@gmail.com>
2013-11-11 20:23:23 +0100

    added serialization tests

---
+++ Tests/SerializationTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Security.AccessControl;
+using System.Security.Principal;
+using Sitecore.Data;
+using Sitecore.Data.Serialization;
+using Xunit;
+
+namespace Sitecore.Fakes.Tests
+{
+    public class SerializationTests
+    {
+        [Fact]
+        public void CanSerializeItemToDisk()
+        {
+            //// Arrange
+            var fakeItem = new FakeItem(new FieldList());
+            var location = Directory.GetCurrentDirectory();
+            var x = new DirectoryInfo(Path.Combine(location, "x"));
+
+            x.Create();
+
+            //// Act
+            Manager.DumpItem(x.FullName, fakeItem);
+
+            //// Assert
+        }
+    }
+}
